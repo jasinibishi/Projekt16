1b037fc3379b61af9b9187e63d4017fbd381c2a0
Jasin Ibishi <jasinibishi@gmail.com>
2024-01-22 23:44:21 +0100

    first commit

---
+++ src/Return.java
@@ -0,0 +1,14 @@
+
+public class Return {
+
+	public static void main(String[] args) {
+			int x = 10;
+			int y= myFunc(x);
+			System.out.println(y);
+			}
+			public static int myFunc(int x){
+				return x*3;
+			}
+            
+	}
+
